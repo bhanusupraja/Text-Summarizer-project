644bf4b4396fdc360ae63828c71be9c713cef827
bhanusupraja99 <bhanusupraja99@gmail.com>
2024-08-05 23:11:55 -0400

    added new changes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "Bhanu Supraja"
+AUTHOR_USER_NAME = "bhanusupraja"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "bhanusupraja99@gmail.com"
 
